74bf3cd65117ee9773a48e67e17ab1f948efeaa3
Lawrence Wu <lawwu@users.noreply.github.com>
2023-09-02 15:25:56 -0700

    add random button

--- app.py
+++ app.py
@@ -2,17 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import json
+import random
 
 st.title('LLM Impact on Jobs')
 st.write('''
 Used llama2-13b-chat to assess the impact of LLMs on jobs. The set of job titles is 
 ONET's list of occupations, which has 1,016 unique titles.
 ''')
-
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-# INPUT_DATA = 'data/list_onet_titles_llm_risk_all.json'
 
 @st.cache_data
 def load_data():
@@ -21,13 +17,29 @@ def load_data():
     data_json = json.loads(data)
     return data_json
 
+# Load Data
 df_onet_titles = pd.read_excel("./data/db_27_3_excel/Occupation Data.xlsx")
 list_onet_titles = list(df_onet_titles['Title'])
 data = load_data()
 
-# Create a dropdown select box
-selected_title = st.selectbox("Choose a job title", list_onet_titles)
-# get index of selected title
-index = list_onet_titles.index(selected_title)
-st.subheader(f'How will LLMs affect {selected_title}?')
+# Initialize or get state
+if 'selected_title' not in st.session_state:
+    st.session_state.selected_title = list_onet_titles[0]
+
+# Create a button for random selection
+if st.button('Select Random Job Title'):
+    st.session_state.selected_title = random.choice(list_onet_titles)
+
+# Create a dropdown select box and bind to session_state
+st.session_state.selected_title = st.selectbox(
+    "Or choose a job title manually",
+    list_onet_titles,
+    index=list_onet_titles.index(st.session_state.selected_title)
+)
+
+# Get index of selected title
+index = list_onet_titles.index(st.session_state.selected_title)
+
+# Display results
+st.subheader(f'How will LLMs affect {st.session_state.selected_title}?')
 st.write(data[index]['generation'])
